e4686134662bcb1cabfa958fbd500bcaaff06a6f
90dego <dmontdr6@gmail.com>
2026-08-28 15:20:02 -0700

    Create or_gate_design.sv

---
+++ or_gate_design.sv
@@ -0,0 +1,11 @@
+// 2-input OR Gate Design
+module or_gate(
+  input logic a,
+  input logic b,
+  output logic y
+);
+  
+  // Continuous assignment modeling the AND logic
+  assign y = a | b;
+
+endmodule
