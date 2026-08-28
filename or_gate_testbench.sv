8144d15e2db30fde7a49736e3919e54db167889d
90dego <dmontdr6@gmail.com>
2026-08-28 15:21:56 -0700

    Create or_gate_testbench.sv

---
+++ or_gate_testbench.sv
@@ -0,0 +1,39 @@
+// 2-input OR Gate Testbench
+module or_gate_tb;
+  
+  // Testbench signals to drive inputs and observe and observe outputs
+  logic a;
+  logic b;
+  logic y;
+  
+  // Instantiate the Unit Under Test (UUT)
+  or_gate uut (
+    .a(a),
+    .b(b),
+    .y(y)
+  );
+  
+  // Stimulus process
+  initial begin
+    
+    // Case 1: a = 0, b = 0 -> y = 0
+    a = 0; b = 0;
+    #10;
+    
+    // Case 2: a = 0, b = 1 -> y = 1
+    a = 0; b = 1;
+    #10;
+    
+    // Case 3: a = 1, b = 0 -> y = 1
+    a = 1; b = 0;
+    #10;
+    
+    // Case 4: a = 1, b = 1 -> y = 1
+    a = 1; b = 1;
+    #10;
+    
+    // End simulation
+    $finish;
+  end
+ 
+endmodule
