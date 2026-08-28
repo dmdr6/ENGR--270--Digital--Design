a96e2f2c47ee8af281dd313c59604a970443b032
90dego <dmontdr6@gmail.com>
2026-08-28 15:28:06 -0700

    Create not_gate_testbench.sv

---
+++ not_gate_testbench.sv
@@ -0,0 +1,29 @@
+// NOT Gate Testbench
+module not_gate_tb;
+  
+  // Testbench signals to drive inputs and observe and observe outputs
+  logic a;
+  logic y;
+  
+  // Instantiate the Unit Under Test (UUT)
+  not_gate uut (
+    .a(a),
+    .y(y)
+  );
+  
+  // Stimulus process
+  initial begin
+    
+    // Case 1: a = 0 -> y = 1
+    a = 0;
+    #10;
+    
+    // Case 2: a = 1 -> y = 0
+    a = 1;
+    #10;
+    
+    // End simulation
+    $finish;
+  end
+ 
+endmodule
