514ed100994c7c37e365759f6f5976d6f7f3fa19
90dego <dmontdr6@gmail.com>
2026-08-28 15:16:56 -0700

    Create and_gate_design.sv

---
+++ and_gate_design.sv
@@ -0,0 +1,11 @@
+// 2-input AND Gate Design
+module and_gate(
+  input logic a,
+  input logic b,
+  output logic y
+);
+  
+  // Continuous assignment modeling the AND logic
+  assign y = a & b;
+
+endmodule;
