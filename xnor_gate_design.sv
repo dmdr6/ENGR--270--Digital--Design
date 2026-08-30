a13653478ad91d2bf52b6e6e6bde74fafd64b43d
90dego <dmontdr6@gmail.com>
2026-08-29 18:45:57 -0700

    Create xnor_gate_design.sv

---
+++ xnor_gate_design.sv
@@ -0,0 +1,11 @@
+// 2-input XNOR Gate Design
+module xnor_gate (
+  input logic a,
+  input logic b,
+  output logic y
+);
+  
+  // Continuous assignment modeling the XNOR logic
+  assign y = ~ (a ^ b);
+  
+endmodule
